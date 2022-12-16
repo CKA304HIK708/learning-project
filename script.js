9ee2fed60d6581b887ec49391fd3e46c9f00f16f
Artem <ar.art.baryshnikov@gmail.com>
2022-12-16 12:13:32 +0300

    Добавление цикла и условий

--- script.js
+++ script.js
@@ -8,7 +8,35 @@ const personalMovieDB = {
     privat: false
 };
 
-let showMovie = prompt("Один из последних просмотренных фильмов?", ""),
-    metaScore = +prompt("На сколько оцениваете его?", "");
+for( let i = 0; i < 2; i++){
 
-    personalMovieDB.movies[showMovie] = metaScore;
+     let showMovie = prompt("Один из последних просмотренных фильмов?", ""),
+         metaScore = +prompt("На сколько оцениваете его?", "");
+
+        personalMovieDB.movies[showMovie] = metaScore;
+
+        if(showMovie && metaScore && showMovie.length < 50) {
+        console.log("Готово");
+        } else {
+        console.log("Отмена");
+        i--;
+        }
+}
+
+if(personalMovieDB.count < 10){
+    console.log("Просмотренно мало фильмов");
+}else if(personalMovieDB.count >= 10 && personalMovieDB.count <=30){
+    console.log("Классический зритель");
+}else if(personalMovieDB.count > 30){
+    console.log("Киноман");
+}else{
+    console.log("Ошибка");
+}
+
+    
+    
+
+   
+
+
+
